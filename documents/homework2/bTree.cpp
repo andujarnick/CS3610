b0f93205497aa8f6d2e4e1c99cf0f87d1a7f4dd2
andujarnick <na188318@ohio.edu>
2021-03-16 21:50:04 -0400

    some code added

--- documents/homework2/bTree.cpp
+++ documents/homework2/bTree.cpp
@@ -1,1 +1,62 @@
-//enter the code here
+#include <iostream>
+#include "bTree.h"
+using namespace std;
+
+template<class elemType>
+binaryTreeType<elemType>::binaryTreeType()
+{
+  root = NULL;
+}
+template<class elemType>
+bool binaryTreeType<elemType>::isEmpty()
+{
+  return (root == NULL);
+}
+template<class elemType>
+void binaryTreeType<elemType>::inorderTraversal()
+{
+  inorder(root);
+}
+template<class elemType>
+void binaryTreeType<elemType>::preorderTraversal()
+{
+  preorder(root);
+}
+template<class elemType>
+void binaryTreeType<elemType>::postorderTraversal()
+{
+  postorder(root);
+}
+template<class elemType>
+int binaryTreeType<elemType>::treeHeight()
+{
+  return height(root);
+}
+template<class elemType>
+int binaryTreeType<elemType>::treeNodeCount()
+{
+  return nodeCount(root);
+}
+template<class elemType>
+int binaryTreeType<elemType>::treeLeavesCount()
+{
+  return leavesCount(root);
+}
+
+template<class elemType>
+int binaryTreeType<elemType>::leavesCount(nodeType<elemType> *p){
+    //returns nothing if the node is at the end of the tree
+    if(node == NULL)
+        {return 0;}
+
+    else{
+        //returns 1 if the node is at a leaf
+        if((p -> rlink == NULL) && (p -> llink == NULL)){
+            return 1;
+        }
+        //continues down the list if it is not at a leaf or the end
+        else{
+            return leavesCount(p -> rlink) + leavesCount(p -> llink);
+        }
+    }
+}
--- documents/homework2/bTree.h
+++ documents/homework2/bTree.h
@@ -13,13 +13,14 @@ class binaryTreeType
 {
 public:
   const binaryTreeType<elemType>& operator=(const binaryTreeType<elemType>&);//overload the assignment operator
-  bool isEmpty() const;//returns whether the tree is empty
-  void inorderTraversal() const;//does an inordertreaversal of the tree
-  void preorderTraversal() const;//function to do a postorder traversal of the binary
-  
-  int treeHeight() const;
-  int treeNodeCount() const;
-  int treeLeavesCount() const;
+  bool isEmpty();//returns whether the tree is empty
+  void inorderTraversal();//does an inordertreaversal of the tree
+  void preorderTraversal();//function to do a postorder traversal of the binary
+  void postorderTraversal();//function that do a postorder traversal of the binary tree
+
+  int treeHeight();
+  int treeNodeCount();
+  int treeLeavesCount();
   void destroytree();//deallocates the memory space ocupied by the binary tree
   
   binaryTreeType(const binaryTreeTrype<elemType>* otherTree);//copy constructor
@@ -32,4 +33,13 @@ public:
     binaryTreeNode<elemType> *root;
     
   private:
-    void copyTree(binaryTreeNode<elemType>* &copiedTreeRoot,
+    void copyTree(binaryTreeNode<elemType>* &copiedTreeRoot, nodeType<elemType>* &otherTreeRoot);
+    void destroy(nodeType<elemType> *p);
+    void inorder(nodeType<elemType>* p);
+    void postorder(nodeType<elemType> *p);
+    int height(nodeType<elemType> * p);
+    int max(int x, int y);
+    int nodeCount(nodeType<elemType> *p);
+    int leavesCount(nodeType<elemType> *p);
+};
+
